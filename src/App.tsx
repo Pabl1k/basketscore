2d25319a7553c45333bcfa6dba66bf5a33c7e246
Pabl1k <poiumazaya@gmail.com>
2023-04-01 20:22:23 +0300

    Installed react-query and react-router-dom and connected to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import LeagueView from "./views/LeagueView/LeagueView";
+import "./App.scss";
+
 const App = () => {
-  return <div>Lorem ipsum dolor sit amet.</div>;
+
+  return (
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="/:selectedLeague" element={<LeagueView />} />
+        <Route path="/:selectedLeague/:selectedTeam" element={<h1>Team page</h1>} />
+
+        <Route path="*" element={<h1>Page doesn't exist</h1>} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
