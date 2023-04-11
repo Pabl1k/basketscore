7aca0d8287a852c329d06b8914252d01db9020d1
Pabl1k <poiumazaya@gmail.com>
2023-04-11 13:27:29 +0300

    Updated Header, data removed to model, app logo component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,88 +1,10 @@
-import { FC } from "react";
 import ShowMore from "../ShowMore/ShowMore";
 import Icon from "../Icon/Icon";
 import "./Header.scss";
 import { useLayout } from "../../hooks/useLayout";
 import AppLogo from "../AppLogo/AppLogo";
-import { calculateItemWidth } from "./model";
+import { calculateItemWidth, initialHeader } from "./model";
 import { Link } from "react-router-dom";
-
-const initialHeader = [
-  {
-    id: 12,
-    name: "NBA",
-    type: "League",
-    logo: "https://media-1.api-sports.io/basketball/leagues/12.png",
-    country: {
-      id: 5,
-      name: "USA",
-      code: "US",
-      flag: "https://media-3.api-sports.io/flags/us.svg"
-    }
-  },
-  {
-    id: 120,
-    name: "Euroleague",
-    type: "cup",
-    logo: "https://media-1.api-sports.io/basketball/leagues/120.png",
-    country: {
-      id: 55,
-      name: "Europe",
-      code: null,
-      flag: null
-    }
-  },
-  {
-    id: 194,
-    name: "Eurocup",
-    type: "cup",
-    logo: "https://media-1.api-sports.io/basketball/leagues/194.png",
-    country: {
-      id: 55,
-      name: "Europe",
-      code: " ",
-      flag: "https://media-1.api-sports.io/flags/ .svg"
-    }
-  },
-  {
-    id: 202,
-    name: "Champions League",
-    type: "cup",
-    logo: "https://media-3.api-sports.io/basketball/leagues/202.png",
-    country: {
-      id: 55,
-      name: "Europe",
-      code: " ",
-      flag: "https://media-2.api-sports.io/flags/ .svg"
-    }
-  }
-  // {
-  //   id: 208,
-  //   name: "Champions League",
-  //   type: "cup",
-  //   logo: "https://media-3.api-sports.io/basketball/leagues/202.png",
-  //   country: {
-  //     id: 55,
-  //     name: "Europe",
-  //     code: " ",
-  //     flag: "https://media-2.api-sports.io/flags/ .svg"
-  //   }
-  // },
-  // {
-  //   id: 205,
-  //   name: "Champions League",
-  //   type: "cup",
-  //   logo: "https://media-3.api-sports.io/basketball/leagues/202.png",
-  //   country: {
-  //     id: 55,
-  //     name: "Europe",
-  //     code: " ",
-  //     flag: "https://media-2.api-sports.io/flags/ .svg"
-  //   }
-  // }
-];
-
-interface Props {}
 
 const getLeagueLogo = (id: number, name: string, logo: string, sm: boolean) => {
   switch (id) {
@@ -113,12 +35,8 @@ const getLeagueLogo = (id: number, name: string, logo: string, sm: boolean) => {
   }
 };
 
-const Header: FC<Props> = ({}) => {
+const Header = () => {
   const { sm } = useLayout(document.body);
-
-  const getPath = (name: string) => {
-    return `/${encodeURIComponent(name.toLowerCase())}`;
-  };
 
   return (
     <header>
@@ -129,7 +47,7 @@ const Header: FC<Props> = ({}) => {
             {initialHeader.map((league) => (
               <Link
                 key={league.id}
-                to={getPath(league.name)}
+                to={league.id.toString()}
                 className="header__league"
                 style={{ width: calculateItemWidth(initialHeader.length) }}
                 title={league.name}
--- src/components/Header/model.ts
+++ src/components/Header/model.ts
@@ -1,3 +1,54 @@
+export const initialHeader = [
+  {
+    id: 12,
+    name: "NBA",
+    type: "League",
+    logo: "https://media-1.api-sports.io/basketball/leagues/12.png",
+    country: {
+      id: 5,
+      name: "USA",
+      code: "US",
+      flag: "https://media-3.api-sports.io/flags/us.svg"
+    }
+  },
+  {
+    id: 120,
+    name: "Euroleague",
+    type: "cup",
+    logo: "https://media-1.api-sports.io/basketball/leagues/120.png",
+    country: {
+      id: 55,
+      name: "Europe",
+      code: null,
+      flag: null
+    }
+  },
+  {
+    id: 194,
+    name: "Eurocup",
+    type: "cup",
+    logo: "https://media-1.api-sports.io/basketball/leagues/194.png",
+    country: {
+      id: 55,
+      name: "Europe",
+      code: " ",
+      flag: "https://media-1.api-sports.io/flags/ .svg"
+    }
+  },
+  {
+    id: 202,
+    name: "Champions League",
+    type: "cup",
+    logo: "https://media-3.api-sports.io/basketball/leagues/202.png",
+    country: {
+      id: 55,
+      name: "Europe",
+      code: " ",
+      flag: "https://media-2.api-sports.io/flags/ .svg"
+    }
+  }
+];
+
 export const calculateItemWidth = (itemsCount: number) => {
   return `${100 / itemsCount}%`;
 };
